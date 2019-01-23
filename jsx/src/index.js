81a7265fb414410cc4a437380a69da53f7960239
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-01-23 00:08:18 -0300

    dive in

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+// Import React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create React component
+const App = () => {
+  return <div>Hi There!</div>;
+};
+
+// Take the React component and show it on screen
+ReactDOM.render(<App />, document.getElementById('root'));
