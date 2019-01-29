ba7b5448e85bab7f826e0391e18864da11a180f8
Lautaro Aguirre <32684961+lautaguirre@users.noreply.github.com>
2019-01-28 22:16:18 -0300

    lifecycles

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -9,19 +9,20 @@ class App extends React.Component {
       lat: null,
       errorMessage: ''
     };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  render() {
+  componentDidUpdate() {
+    console.log('updated');
+  }
 
+  render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
