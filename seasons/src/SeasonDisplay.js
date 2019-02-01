2f3630872b1bfacea6b9a19c0cea2cff0b95e1e1
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-01-31 23:37:42 -0300

    63

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>asd</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+
+  return (
+    <div>
+      <h1>{season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'}</h1>
+    </div>);
 };
 
 export default SeasonDisplay;
