35932f59a4d2c8f7b1351b9442d4e3f1244d6b5a
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-03-30 20:17:13 -0300

    redux thunk

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPost } from '../actions';
+
+class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPost();
+  }
+
+  render() {
+    return (
+      <div>postlist</div>
+    );
+  }
+}
+
+export default connect(null, { fetchPost })(PostList);
