0b468febffaec98ccf2c8786eb53cf8cac56e78a
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-04-20 18:38:07 -0300

    translate project (context API)

---
+++ translate/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
+
+import UserCreate from './UserCreate';
+
+class App extends Component {
+  state = {
+    language: 'english',
+  };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
