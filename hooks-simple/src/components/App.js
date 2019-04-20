a1cefc23fc2f1784acb34a9fb23865ffd54748ed
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-04-20 19:21:55 -0300

    created hooks project

---
+++ hooks-simple/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div className="ui container">
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      {resource}
+    </div>
+  );
+};
+
+export default App;
